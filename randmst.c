0277683919c923366a1c1d2d9ac47a8c8b10aae6
elbertgong <elbertgong@college.harvard.edu>
2017-02-19 02:17:29 -0500

    trying to write most confusing code possible

--- randmst.c
+++ randmst.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-// mst0(int numtrials)
-//		Returns the length of a randomly-generated MST
-
+// float random()
+//		Returns a random float between 0 and 1
+float random() {
+	return 0.5;
+}
 
 // timestamp()
 //		Return the current time as a double.
@@ -12,6 +14,94 @@ static inline double timestamp(void) {
     gettimeofday(&tv, NULL);
     return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
+
+// float mst0(int numtrials)
+//		Returns the length of a randomly-generated 0D MST
+
+
+
+
+typedef struct {
+	float x;
+	float y;
+	float dist;
+	unsigned char searched;
+} point2;
+
+typedef struct node2 {
+	point2* p;
+	node2* prev;
+	node2* next;
+}
+
+// void insert2(point2* point, float dist, node2* heap)
+//		Inserts into the heap
+
+// point2* deletemin2(node2* heap)
+//		Deletes min from heap and returns it
+point2* deletemin2(node2* heap) {
+	node2* crawler = heap;
+	node2* minptr = heap;
+	while (crawler != NULL) {
+		if (crawler->p->dist < minptr->p->dist)
+			minptr = crawler;
+		crawler = crawler->next;
+	}
+	// linked list manipulations
+}
+
+// float closer2(point2* p, point2* q, float origdist)
+//		Returns a new distance if the distance between p and q is less than
+//		p's original "distance" and less than k(n), -1 otherwise
+
+
+// float mst2(int numtrials)
+//		Returns the length of a randomly-generated 2D MST
+float mst2(int numpoints) {
+	point2* points = malloc(sizeof(point2) * numpoints); // need to free everything later???
+	for (int i = 0; i < numpoints; i++) {
+		*(points + i).x = random();
+		*(points + i).y = random();
+		*(points + i).dist = 0xffffffff; // are floats 4 bytes???
+		*(points + i).searched = 0;
+	}
+	points[0].dist = 0;
+
+	node2* heap = malloc(sizeof(node2));
+	heap->p = points;
+	heap->prev = NULL;
+	heap->next = NULL;
+
+	while(heap != NULL) {
+		point2* p = deletemin2(heap);
+		for (int i = 0; i < numpoints; i++) {
+			if (!(*points + i).searched) {
+				float length = closer2(p, points + i, p->dist);
+				if (length >= 0) {
+					(points + i)->dist = length;
+					insert2((points + i), length, heap);
+				}
+			}
+		}
+	}
+
+	float total = 0;
+	for (int i = 0; i < numpoints; i++)
+		total += (points + i)->dist;
+	return total;
+}
+
+
+// float mst3(int numtrials)
+//		Returns the length of a randomly-generated 3D MST
+
+
+// float mst4(int numtrials)
+//		Returns the length of a randomly-generated 4D MST
+
+
+
+
 
 
 int main(int argc, char** argv) {
