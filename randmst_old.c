115d64769b154d887ef36bec05fc5cacc44e1a67
elbertgong <elbertgong@college.harvard.edu>
2017-02-22 00:53:54 -0500

    implemented 0D, but it's kind of bad

--- randmst_old.c
+++ randmst_old.c
@@ -1,13 +1,7 @@
 // can you think of a way to combine functions so that we don't have unique ones for each dimension?
-// For instance, insert2 and insert0 are almost the same.. any idea how to integrate them?
-// maybe separate the insert and delete functions into a header file
-
-
-// Big Important issues:
-// need to implement/determine k(numpoints). 
-// need to implement the other dimensions.
-// need to make it faster somehow- the 0D one can't get up to 20000
-// need to comment this up better- my fault.
+// right now it takes a few seconds to do like 20000 with 2 dimensions
+// I'm not sure if this will get to 131072 with 4...
+// Big questions: do we want to switch heap structures? How do we determine k(numpoints)?
 
 
 #include <stdio.h>
@@ -18,143 +12,13 @@
 #include <math.h>
 #include <string.h>
 
-
-typedef struct node0 {
-	int vertex;
-	float dist;
-	struct node0* next;
-} node0;
-
-typedef struct {
-	node0* neighbors;
-	float dist;
-	unsigned char searched;
-} point0;
-
-typedef struct heapnode0 {
-	point0* p;
-	struct heapnode0* prev;
-	struct heapnode0* next;
-} heapnode0;
-
-// Global heap for 2D graph
-static heapnode0* heap0;
-
-// void insert0(point0* point, float dist)
-//		Insert0 into the heap (or updates an existing heap node)
-void insert0(point0* point, float dist) {
-	point->dist = dist;
-	assert(point->searched != 2);
-	if (point->searched == 1) return;
-	point->searched = 1;
-	
-	// actually put a new node into the heap
-	heapnode0* n = malloc(sizeof(heapnode0));
-	n->p = point;
-	n->prev = NULL;
-	if (heap0) {
-		n->next = heap0;
-		heap0->prev = n;
-	}
-	else {
-		n->next = NULL;
-	}
-	heap0 = n;
-}
-
-// point0* deletemin0(node0* heap)
-//		Deletes min from heap and returns it
-point0* deletemin0() {
-	heapnode0* crawler = heap0;
-	heapnode0* minptr = heap0;
-	while (crawler != NULL) {
-		if (crawler->p->dist < minptr->p->dist)
-			minptr = crawler;
-		crawler = crawler->next;
-	}
-
-	// we've found minimum, now extract it from linked list
-	if (minptr->prev) {
-		minptr->prev->next = minptr->next;
-	}
-	else {
-	    heap0 = minptr->next;
-	}
-	if (minptr->next) {
-		minptr->next->prev = minptr->prev;
-	}
-	point0* x = minptr->p;
-	assert (x->searched == 1);
-	x->searched = 2;
-	free(minptr);
-	return x;
-}
-
-
-
 // float mst0(int numpoints)
 //		Returns the length of a randomly-generated 0D MST
 // for this one, generate numpoints random numbers and return max - min
 float mst0(int numpoints) {
-    point0* points = malloc(sizeof(point0) * numpoints);
-	for (int i = 0; i < numpoints; i++) {
-		(points + i)->neighbors = NULL;
-		(points + i)->dist = 10; // i know it's dumb, it just has to be bigger than sqrt(26)
-		(points + i)->searched = 0;
-		for (int j = 0; j < i; j++) {
-			float dist_ij = (float) rand() / RAND_MAX;
-			if (dist_ij < 1) { // later change to k(numpoints)
-				node0* new = malloc(sizeof(node0));
-				new->vertex = i;
-				new->dist = dist_ij;
-				if ((points + j)->neighbors) {
-					new->next = (points + j)->neighbors;
-				}
-				else {
-					new->next = NULL;
-				}
-				(points + j)->neighbors = new;
-			}
-		}
-	}
-    points[0].dist = 0;
-    points[0].searched = 1;
-
-    heap0 = malloc(sizeof(heapnode0));
-	heap0->p = points;
-	heap0->prev = NULL;
-	heap0->next = NULL;
-
-	while(heap0 != NULL) {
-		point0* p = deletemin0();
-		// printf("newmin %f %f %f %d\n", p->x, p->y, p->dist, p->searched);
-		
-		// look for new edges and potentially insert them into linked list
-		while (p->neighbors) {
-			point0* neighbor = points + p->neighbors->vertex;
-			if (neighbor->searched == 0 || neighbor->searched == 1) {
-				if (p->neighbors->dist < neighbor->dist)
-					insert0(neighbor, p->neighbors->dist);
-			}
-			// remove the neighbor from adjacency list
-			node0* temp = p->neighbors;
-			p->neighbors = temp->next;
-			free(temp);
-		}
-	}
-
-	float total = 0;
-	for (int i = 0; i < numpoints; i++) {
-		assert((points + i)->searched == 2);
-		total += (points + i)->dist;
-	}
-	free(points);
-	return total;
+    printf("mst0 not implemented\n");
+    return 0;
 }
-
-
-
-
 
 typedef struct {
 	float x;
